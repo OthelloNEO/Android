c5a31c6a3479a349bbc1dc6b46b37d5fc9aeaf88
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2017-03-11 08:15:41 +0900

    Check point

--- app/src/main/java/com/othello/neo/android/SelectMenuActivity.java
+++ app/src/main/java/com/othello/neo/android/SelectMenuActivity.java
@@ -1,12 +1,19 @@
 package com.othello.neo.android;
 
 import android.app.Activity;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 /**
  * Created by Tomoyuki on 2017/03/04.
@@ -17,19 +24,54 @@ public class SelectMenuActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_menu);
-        //setContentView(R.layout.activity_main);
-
         Log.d("activity", "intent SelectMenuActivity");
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+
+        //ViewPager viewPager = (ViewPager)findViewById(R.id.view_pager);
+        //viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+
+        //tabLayout.setupWithViewPager(viewPager);
+
     }
 
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = null;
+
+            switch (position) {
+                case 0:
+                    Log.d("activity", "Page 0");
+                    //fragment = new FirstFragment();
+                    break;
+                case 1:
+                    Log.d("activity", "Page 1");
+                    //fragment = new SecondFragment();
+                    break;
+                case 2:
+                    Log.d("activity", "Page 2");
+                    //fragment = new ThirdFragment();
+                    break;
+                default:
+                    break;
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "タブ"+ (position+1);
+        }
     }
+
 }
